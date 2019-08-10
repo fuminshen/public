9568627aed5e7d75d3fc917d029a2731b34412b8
fumin <fumin@DESKTOP-PB0NOUG>
2019-08-10 10:15:19 +0800

    1

--- role_demo/src/main/java/com/fumin/role/demo/ShiroConfig.java
+++ role_demo/src/main/java/com/fumin/role/demo/ShiroConfig.java
@@ -43,8 +43,6 @@ public class ShiroConfig {
 		filterChainDefinitionMap.put("/plugins/**", "anon");
 		filterChainDefinitionMap.put("/loginAction", "anon");
 		
-		filterChainDefinitionMap.put("/user/**", "authc,roles[user]");
-		filterChainDefinitionMap.put("/order/new", "roles[admin]");
 		//配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了
 		//filterChainDefinitionMap.put("/logout", "logout");
 		//<!-- 过滤链定义，从上向下顺序执行，一般将/**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
